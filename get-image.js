15d1b50d4c8b946fa38de72092d6e850925688d0
Saoud Al Montafke <81016351+Al-Montafke@users.noreply.github.com>
2024-11-29 06:21:04 +0300

    Update get-image.js

--- get-image.js
+++ get-image.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 async function get_image(username, filename) {
-    const response = await fetch(`http://localhost:3000/user/${username}/images/${filename}`);
+    const response = await fetch(`${process.env.pre_link}/user/${username}/images/${filename}`);
     if (response.status === 404) return null;
 
     return response.url;
